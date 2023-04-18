8d1e8ed9712b40a395347b50960b1b26ffb70a0e
MarsBroshok96 <marsbroshok1996@gmail.com>
2023-04-18 23:37:36 +0600

    Add </> to all routes for hexlet-test

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.StatusListView.as_view(), name='status_list'),
-    path('create', views.StatusCreateView.as_view(), name='create_status'),
-    path('<int:pk>/update',
+    path('create/', views.StatusCreateView.as_view(), name='create_status'),
+    path('<int:pk>/update/',
          views.StatusUpdateView.as_view(),
          name='update_status'
          ),
-    path('<int:pk>/delete',
+    path('<int:pk>/delete/',
          views.StatusDeleteView.as_view(),
          name='delete_status'
          ),
